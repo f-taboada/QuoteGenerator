a8a93166141ccc37818e9e19cf49f17b950211f6
f-taboada <facundotaboada02@gmail.com>
2024-01-10 08:30:30 -0300

    More styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
 //Components
-import { BackgroundImage1, BackgroundImage2, FooterCon, FooterLink, GradientBackgroundCon, RedSpan } from '@/components/QuoteGenerator/QuoteGeneratorElements'
+import { BackgroundImage1, BackgroundImage2, FooterCon, FooterLink, GenerateQuoteButton, GenerateQuoteButtonText, GradientBackgroundCon, QuoteGeneratorCon, QuoteGeneratorInnerCon, QuoteGeneratorSubTitle, QuoteGeneratorTitle, RedSpan } from '@/components/QuoteGenerator/QuoteGeneratorElements'
 //Assets
 import Clouds1 from '@/assets/cloud-and-thunder.png'
 import Clouds2 from '@/assets/cloudy-weather.png'
@@ -23,6 +23,34 @@ export default function Home() {
       {/* Background */}
       <GradientBackgroundCon>
 
+        {/* Quote Generator Modal Pop-Up */} 
+        {/* <QuoteGeneratorModal>
+
+        </QuoteGeneratorModal> */}
+
+        {/* Quote Generator */} 
+        <QuoteGeneratorCon>
+          <QuoteGeneratorInnerCon>
+
+            <QuoteGeneratorTitle>
+              Daily Inspiration Generator
+            </QuoteGeneratorTitle>
+
+            <QuoteGeneratorSubTitle>
+            Looking for a splash of inspiration? Generate a quote card with a random inspirational quote provided by <FooterLink href="https://zenquotes.io/" target="_blank" rel="noopener noreferrer">ZenQuotes API</FooterLink>.
+            </QuoteGeneratorSubTitle>
+
+            <GenerateQuoteButton>
+              <GenerateQuoteButtonText onClick={null}>
+                Make a Quote
+              </GenerateQuoteButtonText>
+            </GenerateQuoteButton>
+
+          </QuoteGeneratorInnerCon>
+        </QuoteGeneratorCon>
+
+
+        {/* Background Images*/} 
         <BackgroundImage1
           src={Clouds1}
           height="300"
@@ -35,14 +63,14 @@ export default function Home() {
           alt="cloudybackground1"
           />          
 
-         <FooterCon>
-            <>
-              Quotes Generated: {numberOfQuotes}
-              <br />
-              Developed with <RedSpan>love</RedSpan> by <FooterLink href="https://github.com/f-taboada"
-              target="_blank" rel="noopener noreferrer"> @FacundoTaboada </FooterLink>  
-            </>
-
+        {/* Footer*/} 
+        <FooterCon>
+          <>
+            Quotes Generated: {numberOfQuotes}
+            <br />
+            Developed with <RedSpan>love</RedSpan> by <FooterLink href="https://github.com/f-taboada"
+            target="_blank" rel="noopener noreferrer"> @FacundoTaboada </FooterLink>  
+          </>
          </FooterCon>
       </GradientBackgroundCon>
     </>
