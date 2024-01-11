3f5eb075728622a68cea19062023887fd147e23c
f-taboada <facundotaboada02@gmail.com>
2024-01-11 12:48:30 -0300

    amplify config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+// AWS Imports
+import { Amplify } from 'aws-amplify';
+import amplifyconfig from '../src/amplifyconfiguration.json';
+Amplify.configure(amplifyconfig);
+
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
+
+ 
